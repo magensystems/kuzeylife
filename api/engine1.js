2b9ef50477ecc0f42aa087110856338ac511540d
magensystems <magensystems.webar@gmail.com>
2025-12-25 16:13:41 +0300

    Update engine1.js

--- api/engine1.js
+++ api/engine1.js
@@ -54,7 +54,7 @@ const MODEL_DB = {
 };
 
 // Varsayılan HDR Sahnesi
-const DEFAULT_ENV = "environments/studio_lite.hdr";
+const DEFAULT_ENV = "environments/studio.hdr";
 
 
 const client = new S3Client({
